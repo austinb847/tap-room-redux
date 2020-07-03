f5a1d63804fb20eeb95d112fdfb37079d20ed3fa
austinb847 <austinb847@icloud.com>
2020-07-03 14:57:22 -0700

    update keg detail component to render the selected keg and display all the keg content

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -2,16 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function KegDetail(props) {
+  const { keg } = props;
   return (
     <React.Fragment>
-      <h1>Keg Detail</h1>
+      <h1>{keg.name}</h1>
+      <ul>
+        <li>Brand: {keg.brand}</li>
+        <li>Price: {keg.price}</li>
+        <li>Alcohol Content: {keg.alcoholContent}</li>
+      </ul>
       <hr/>
     </React.Fragment>
   )
 }
 
 KegDetail.propTypes = {
-
+  keg: PropTypes.object
 }
 
 export default KegDetail
