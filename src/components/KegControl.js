aca0d8d69b22a6786bbbab764680bf19dc84379c
austinb847 <austinb847@icloud.com>
2020-07-03 12:13:56 -0700

    add masterKegList, formVisibleOnPage, and pints propertys to state and pass down as props to keglist component

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
 import NewKegForm from './NewKegForm';
+import KegList from './KegList';
 
 class KegControl extends Component {
   constructor(props) {
     super(props)
   
     this.state = {
-       
+       formVisibleOnPage: false,
+       masterKegList: [],
+       pints: 124
     }
   }
   
 
   render() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if(this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewKegForm />;
+      buttonText = "Return to Keg List";
+    } else {
+      currentlyVisibleState = <KegList kegList={this.state.masterKegList}/>
+      buttonText = "Add New Keg";
+    }
     return (
      <React.Fragment>
-       <NewKegForm />
+       
      </React.Fragment>
     )
   }
