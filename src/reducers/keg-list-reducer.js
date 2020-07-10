6f0d50fca3b08d85ebf9cd00cae9bbcb2963fddd
austinb847 <austinb847@icloud.com>
2020-07-10 11:34:44 -0700

    succesfully add new kegs to the master keg list

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
-
+  const {name, brand, price, alcoholContent, id, pints} = action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return {
+        ...state,
+        [id]: { 
+          name: name,
+          brand: brand,
+          price: price,
+          alcoholContent: alcoholContent,
+          id: id,
+          pints: pints
+        }
+      };
+    default:
+      return state;
+  }
 };
