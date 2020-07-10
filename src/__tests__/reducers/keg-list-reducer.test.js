148b4fa4d80a11f65542ccf2f92febb0cf9e8ffa
austinb847 <austinb847@icloud.com>
2020-07-10 11:23:23 -0700

    meaningful fail test for adding a keg to the master keg list

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,43 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+  let action;
+  const keg = {
+    name: 'Test keg',
+    brand: 'Test Brand',
+    price: 'Test price',
+    alcoholContent: 'Test content',
+    id: 1,
+    pints: 124
+  }
+
   test('Should return default state if no action type is specified', () => {
     expect(kegListReducer({}, {type: null})).toEqual({});
   });
+
+  test('Should add a new keg to the keg list', () => {
+    const { name, brand, price, alcoholContent, id, pints } = keg;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      id: id,
+      pints: pints
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        id: id,
+        pints: pints
+      }
+    })
+  });
+
+
 });
