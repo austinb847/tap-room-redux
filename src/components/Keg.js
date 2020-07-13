0380b68d3ad0fac9fa297fe2c8338b3e1805458d
austinb847 <austinb847@icloud.com>
2020-07-12 20:54:35 -0700

    add icons, change background color

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { DeleteFilled } from '@ant-design/icons';
+import {ReactComponent as BeerIcon} from './beer.svg';
 
 function Keg(props) {
+  
   return (
     <React.Fragment>
       <div className="column">
         <div className="keg-item">
           <h3 onClick = {() => props.whenKegClicked(props.id)} className='keg'>{props.name} - {props.brand}</h3>
           <p>Total Pints: {props.pints}</p>
-          <button onClick= {() => props.whenKegSold({name: props.name, brand: props.brand, price: props.price, alcoholContent: props.alcoholContent, 
-                                                      id: props.id, pints: props.pints})}>Sell Pint</button>
+          {/* <button onClick= {() => props.whenKegSold({name: props.name, brand: props.brand, price: props.price, alcoholContent: props.alcoholContent, 
+                                                      id: props.id, pints: props.pints})}>Sell Pint</button> */}
           <br/>
-          <button onClick={() => props.whenKegDeleted(props.id)}>Delete Keg</button>
-          
+          {/* <button onClick={() => props.whenKegDeleted(props.id)}>Delete Keg</button> */}
+          <BeerIcon className="add-keg-button" onClick= {() => props.whenKegSold({name: props.name, brand: props.brand, price: props.price, alcoholContent: props.alcoholContent, 
+                                                      id: props.id, pints: props.pints})}/>
+          <DeleteFilled className="delete-button" onClick={() => props.whenKegDeleted(props.id)}/>
         </div>
       </div>
     </React.Fragment>
